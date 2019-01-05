62432dbaa0537539388e3c2b930564ccb456063c
septianaana <45724156+septianaana@users.noreply.github.com>
2019-01-05 11:32:26 +0700

    Add files via upload

---
+++ e=mc2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    int E,m,c;
+    cout<<"\aPROGRAM MENGHITUNG RUMUS E=mc2\n"<<endl;
+    cout<<"masukan nilai m:";cin>>m;
+    cout<<"masukan nilai c:";cin>>c;
+
+    E=m*c*c;
+    cout<<"E="<<E<<endl;
+    cout<<"hasil nilai E adalah "<<E;
+    std::cout<<" \n";
+
+    std::cout<<"nama : Hamdan yuapi \n";
+    std::cout<<"nim  : 311810132";
+    return 0;
+}
